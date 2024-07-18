a5b91f45b81b7f8c7b95b5e90427cc7613d661a7
gunners <chakagae123@naver.com>
2024-07-19 01:21:50 +0900

    * springMVC1 끝

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -95,11 +96,20 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add") //@ModelAttribute도 생략가능
+//    @PostMapping("/add") //@ModelAttribute도 생략가능
     public String addItemV5(Item item) {
 
         itemRepository.save(item);
         return "redirect:/basic/items/" + item.getId();
+    }
+
+    @PostMapping("/add") //@ModelAttribute도 생략가능
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
